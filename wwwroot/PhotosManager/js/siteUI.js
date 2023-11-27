26540af8213539dcfe74a8c4c170ed271a9c0693
Pogod171 <127141703+Pogod171@users.noreply.github.com>
2023-11-26 19:52:32 -0500

    Initial commit

---
+++ wwwroot/PhotosManager/js/siteUI.js
@@ -0,0 +1,187 @@
+let contentScrollPosition = 0;
+Init_UI();
+console.log("Allo");
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Views rendering
+function showWaitingGif() {
+  eraseContent();
+  $("#content").append(
+    $(
+      "<div class='waitingGifcontainer'><img class='waitingGif' src='images/Loading_icon.gif' /></div>'"
+    )
+  );
+}
+function eraseContent() {
+  $("#content").empty();
+}
+function saveContentScrollPosition() {
+  contentScrollPosition = $("#content")[0].scrollTop;
+}
+function restoreContentScrollPosition() {
+  $("#content")[0].scrollTop = contentScrollPosition;
+}
+function updateHeader(titre = "") {
+  $("#header").empty();
+  $("#header").append(
+    $(`<img src="favicon.ico" class="appLogo" alt="" title="Gestionnaire de favoris">
+        <h4 id="actionTitle">${titre}</h4>
+        <div class="dropdown ms-auto">
+            <div data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="cmdIcon fa fa-ellipsis-vertical"></i>
+            </div>
+            <div class="dropdown-menu noselect" id="DDMenu">
+            <div class="dropdown-item menuItemLayout" id="aboutCmd">
+                <i class="menuIcon fa fa-info-circle mx-2"></i> À propos...
+            </div>
+        </div>
+    </div>`)
+  );
+}
+function renderLoginPage(Email = "", EmailError = "", passwordError = "") {
+  timeout();
+  saveContentScrollPosition();
+  eraseContent();
+  updateHeader("Connexion", "login");
+  $("#content").append(
+    $(`<div class="content" style="text-align:center">
+        <h3></h3>
+        <form class="form" id="loginForm">
+            <input type='email'
+                name='Email'
+                class="form-control"
+                required
+                RequireMessage = 'Veuillez entrer votre courriel'
+                InvalidMessage = 'Courriel invalide'
+                placeholder="adresse de courriel"
+                value='${Email}'>
+            <span style='color:red'>${EmailError}</span>
+            <input type='password'
+                name='Password'
+                placeholder='Mot de passe'
+                class="form-control"
+                required
+                RequireMessage = 'Veuillez entrer votre mot de passe'>
+            <span style='color:red'>${passwordError}</span>
+            <input type='submit' name='submit' value="Entrer" class="form-control btn-primary">
+        </form>
+        <div class="form">
+            <hr>
+            <button class="form-control btn-info" id="createProfilCmd">Nouveau compte</button>
+        </div>
+    </div>`)
+  );
+}
+
+function renderInscription() {
+  timeout();
+  saveContentScrollPosition();
+  eraseContent();
+  updateHeader("Inscription", "inscription");
+  $("#content").append(
+    $(`<form class="form" id="createProfilForm"'>
+    <fieldset>
+    <legend>Adresse ce courriel</legend>
+    <input type="email"
+    class="form-control Email"
+    name="Email"
+    id="Email"
+    placeholder="Courriel"
+    required
+    RequireMessage = 'Veuillez entrer votre courriel'
+    InvalidMessage = 'Courriel invalide'
+    CustomErrorMessage ="Ce courriel est déjà utilisé"/>
+    <input class="form-control MatchedInput"
+    type="text"
+    matchedInputId="Email"
+    name="matchedEmail"
+    id="matchedEmail"
+    placeholder="Vérification"
+    required
+    RequireMessage = 'Veuillez entrez de nouveau votre courriel'
+    InvalidMessage="Les courriels ne correspondent pas" />
+    </fieldset>
+    <fieldset>
+    <legend>Mot de passe</legend>
+    <input type="password"
+    class="form-control"
+    name="Password"
+    id="Password"
+    placeholder="Mot de passe"
+    required
+    RequireMessage = 'Veuillez entrer un mot de passe'
+    InvalidMessage = 'Mot de passe trop court'/>
+    <input class="form-control MatchedInput"
+    type="password"
+    matchedInputId="Password"
+    name="matchedPassword"
+    id="matchedPassword"
+    placeholder="Vérification" required
+    InvalidMessage="Ne correspond pas au mot de passe" />
+    </fieldset>
+    <fieldset>
+    <legend>Nom</legend>
+    <input type="text"
+    class="form-control Alpha"
+    name="Name"
+    id="Name"
+    placeholder="Nom"
+    required
+    RequireMessage = 'Veuillez entrer votre nom'
+    InvalidMessage = 'Nom invalide'/>
+    </fieldset>
+    <fieldset>
+    <legend>Avatar</legend>
+    <div class='imageUploader'
+    newImage='true'
+    controlId='Avatar'
+    imageSrc='images/no-avatar.png'
+    waitingImage="images/Loading_icon.gif">
+    </div>
+    </fieldset>
+    <input type='submit' name='submit' id='saveUserCmd' value="Enregistrer" class="form-control btn-primary">
+    </form>
+    <div class="cancel">
+    <button class="form-control btn-secondary" id="abortCmd">Annuler</button>
+    </div>`)
+  );
+}
+function renderAbout() {
+  timeout();
+  saveContentScrollPosition();
+  eraseContent();
+  updateHeader("À propos...", "about");
+
+  $("#content").append(
+    $(`
+            <div class="aboutContainer">
+                <h2>Gestionnaire de photos</h2>
+                <hr>
+                <p>
+                    Petite application de gestion de photos multiusagers à titre de démonstration
+                    d'interface utilisateur monopage réactive.
+                </p>
+                <p>
+                    Auteur: Nicolas Chourot
+                </p>
+                <p>
+                    Collège Lionel-Groulx, automne 2023
+                </p>
+            </div>
+        `)
+  );
+}
+
+function Init_UI() {
+  $("#abortCmd").on("click", function () {
+    renderLoginPage();
+  });
+  if (API.retrieveLoggedUser() == null) {
+    renderLoginPage();
+  }
+  $("#aboutCmd").on("click", function () {
+    renderAbout();
+  });
+  $("#createProfilCmd").on("click", function () {
+    renderInscription();
+  });
+}
