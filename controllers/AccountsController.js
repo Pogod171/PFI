007afedad9c3774eefbd3153c8d98a017a825d1f
Fr03 <95039328+Fr03@users.noreply.github.com>
2023-12-04 12:58:24 -0500

    F

--- controllers/AccountsController.js
+++ controllers/AccountsController.js
@@ -155,7 +155,6 @@ export default class AccountsController extends Controller {
                 let foundedUser = this.repository.findByField("Id", user.Id);
                 if (foundedUser != null) {
                     user.Authorizations = foundedUser.Authorizations; // user cannot change its own authorizations
-                    user.VerifyCode = foundedUser.VerifyCode;
                     if (user.Password == '') { // password not changed
                         user.Password = foundedUser.Password;
                     }
